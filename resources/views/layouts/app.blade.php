8b120c292b3ecf7ed56249779d9dabf09c9cf02c
Dikhsantibong <tibongdikhsan@gmail.com>
2025-12-27 02:26:50 +0800

    perbaikan struktur

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,29 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/png">
-        <!--====== Google Fonts ======-->
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
-        <!--====== Flaticon css ======-->
-        <link rel="stylesheet" href="assets/fonts/flaticon/flaticon_agenko.css">
-        <!--====== FontAwesome css ======-->
-        <link rel="stylesheet" href="assets/fonts/fontawesome/css/all.min.css">
-        <!--====== Bootstrap css ======-->
-        <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
-        <!--====== Slick-popup css ======-->
-        <link rel="stylesheet" href="assets/css/plugins/slick.css">
-        <!--====== Magnific-popup css ======-->
-        <link rel="stylesheet" href="assets/css/plugins/magnific-popup.css">
-        <!--====== Default css ======-->
-        <link rel="stylesheet" href="assets/css/default.css">
-        <!--====== Style css ======-->
-        <link rel="stylesheet" href="assets/css/style.css">
-
+    <link rel="stylesheet" href="{{ asset('ashley/css/plugins/bootstrap-grid.css') }}">
+    <!-- font awesome css -->
+    <link rel="stylesheet" href="{{ asset('ashley/css/plugins/font-awesome.min.css') }}">
+    <!-- swiper css -->
+    <link rel="stylesheet" href="{{ asset('ashley/css/plugins/swiper.min.css') }}">
+    <!-- fancybox css -->
+    <link rel="stylesheet" href="{{ asset('ashley/css/plugins/fancybox.min.css') }}">
+    <!-- ashley scss -->
+    <link rel="stylesheet" href="{{ asset('ashley/css/style.css') }}">
 
     <!-- Tempatkan link/style/script global di sini jika perlu -->
 </head>
 <body>
     @yield('content')
+
+    <script src="{{ asset('ashley/js/plugins/jquery.min.js') }}"></script>
+    <!-- swup js -->
+    <script src="{{ asset('ashley/js/plugins/swup.min.js') }}"></script>
+    <!-- swiper js -->
+    <script src="{{ asset('ashley/js/plugins/swiper.min.js') }}"></script>
+    <!-- fancybox js -->
+    <script src="{{ asset('ashley/js/plugins/fancybox.min.js') }}"></script>
+    <!-- gsap js -->
+    <script src="{{ asset('ashley/js/plugins/gsap.min.js') }}"></script>
+    <!-- scroll smoother -->
+    <script src="{{ asset('ashley/js/plugins/smooth-scroll.js') }}"></script>
+    <!-- scroll trigger js -->
+    <script src="{{ asset('ashley/js/plugins/ScrollTrigger.min.js') }}"></script>
+    <!-- scroll to js -->
+    <script src="{{ asset('ashley/js/plugins/ScrollTo.min.js') }}"></script>
+    <!-- ashley js -->
+    <script src="{{ asset('ashley/js/main.js') }}"></script>
 </body>
 </html>
-
